958df05cbb9a307fed420ec9eface002b7d436e7
carter-loremdigital <carter@loremdigital.co>
2025-02-12 01:03:54 -0500

    settings

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,8 @@ import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent # Uncomment for development
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
